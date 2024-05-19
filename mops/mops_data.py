ebdb2ee14e4764bc37a8eb297a1172e729e6444d
dkrivaa <dannykrivaa@hotmail.com>
2024-05-19 18:30:32 +0300

    make mops data

--- mops/mops_data.py
+++ mops/mops_data.py
@@ -3,13 +3,28 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import gspread
 import pandas as pd
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
 
 from general import general_functions
 
 # Opening workbook
 book, available_years = general_functions.openGoogle()
 
+mops_df = pd.DataFrame
+# Extracting mops data for each year
 for year in available_years:
     sheet = book.worksheet(year)
     df = pd.DataFrame(sheet.get_all_records())
-    print(len(df))
+    temp_df = df[df.iloc[:, 3] == '12']
+
+    mops_df = mops_df.append(temp_df, ignore_index=True)
+
+# Make MOPS worksheet in Google file
+try:
+    if book.worksheet('mops'):
+        book.del_worksheet(book.worksheet('mops'))
+except gspread.exceptions.WorksheetNotFound:
+    pass
+
+book.add_worksheet(title='mops', rows=len(mops_df) + 1, cols=len(mops_df.columns) + 1)
+set_with_dataframe(book.worksheet('mops'), mops_df)
