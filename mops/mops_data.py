c2918881daf7c9f86c203562784ef4b92b9c27cd
dkrivaa <dannykrivaa@hotmail.com>
2024-05-19 19:09:42 +0300

    make mops data

--- mops/mops_data.py
+++ mops/mops_data.py
@@ -16,7 +16,7 @@ for year in available_years:
     sheet = book.worksheet(year)
     df = pd.DataFrame(sheet.get_all_records())
     print(year, 'length of year df: ', len(df))
-    temp_df = df[df.iloc[:, 3] == '12']
+    temp_df = df[df.iloc[:, 3] == 12]
 
     print(year, 'length of temp_df (mops data)', len(temp_df))
 
