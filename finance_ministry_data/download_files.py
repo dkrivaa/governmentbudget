f52adde3dd5eab936d72bf93b5310ab01a6ef6be
dkrivaa <dannykrivaa@hotmail.com>
2024-05-19 12:36:58 +0300

    get finance ministry data

---
+++ finance_ministry_data/download_files.py
@@ -0,0 +1,57 @@
+# THIS FILE IS RUN MANUALLY FROM LOCAL PYTHON
+
+import pandas as pd
+import requests
+from io import BytesIO
+
+
+# Getting the raw data from Excel files and returning list with nested lists of dataframes
+# each nested list, representing specific year, consist of 5 dataframes - one for each organization
+try:
+    # 2015-2023 files
+    # url for Excel data from Finance Ministry
+    # These url were taken from 'https://www.gov.il/he/departments/policies/tableau'
+    # url2024 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2024.xls'
+    url2023 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2023.xlsx'
+    url2022 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2022.xlsx'
+    url2021 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_tableau_BudgetData2021.xlsx'
+    url2020 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2020.xlsx'
+    url2019 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2019.xlsx'
+    url2018 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2018.xlsx'
+    url2017 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2017.xlsx'
+    url2016 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2016.xlsx'
+    url2015 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2015.xlsx'
+    # url20241 = 'C:\\Users\danny\Desktop\BudgetData\original2024Before0710231024.xlsx'
+
+    url_list = [url2015, url2016, url2017, url2018, url2019, url2020, url2021, url2022, url2023]
+
+    for url in url_list:
+
+        response = requests.get(url)
+        if response.status_code == 200:
+
+            # Wrap the byte string in a BytesIO object
+            excel_buffer = BytesIO(response.content)
+            # Make dataframe
+            df = pd.read_excel(excel_buffer)
+
+            name = url[-9:-5]
+            df.to_excel(f'{name}.xlsx')
+
+    # 2024 files
+    files = ['finance_ministry_data/before0710original2024.xlsx', 'finance_ministry_data/new2024.xlsx']
+
+    for file in files:
+        # Make dataframe
+        df = pd.read_excel(file)
+
+        if file == files[0]:
+            name = 20241
+        else:
+            name = 2024
+
+        df.to_excel(f'{name}.xlsx')
+
+except:
+    print('somthing went wrong')
+
--- finance_ministry_data/get_data.py
+++ finance_ministry_data/get_data.py
@@ -5,50 +5,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import gspread
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
 import pandas as pd
-import requests
-from io import BytesIO
 
 from general import general_functions
 
 
-# Getting the raw data from Excel files
+# Getting the raw data from Excel files, formatting, dropping unnecessary columns and saving in
+# Google sheet
 def make_Google_sheets(url):
-    try:
-        # Getting data from url
-        response = requests.get(url)
-        print(response.status_code)
-        if response.status_code == 200:
-            # Wrap the byte string in a BytesIO object
-            excel_buffer = BytesIO(response.content)
-            # Make dataframe
-            df = pd.read_excel(excel_buffer)
-
-            # make budget sums into numbers
-            # list of columns to re-format
-            columns_to_format = [30, 31, 32, 33, 34, 35, 36, 37]
-            for column in columns_to_format:
-                # Step 1: Replace values in the format (20) with the corresponding negative value
-                df[df.columns[column]] = df[df.columns[column]].replace(r'\((.*?)\)', r'-\1', regex=True)
-                # Step 2: Replace '-' with 0
-                df[df.columns[column]] = df[df.columns[column]].replace('-', '0', regex=False)
-                # Step 3: Replace NaN values with 0
-                df[df.columns[column]] = df[df.columns[column]].fillna('0')
-                # Convert to integers
-                df[df.columns[column]] = df[df.columns[column]].astype(float)
-
-            # Drop unnecessary columns
-            columnsToDrop = [3, 6, 11, 14, 17, 20, 23, 26]
-            df = df.drop(df.columns[columnsToDrop], axis=1)
-
-            name = url[-9:-5]
-
-            return name, df
-    except Exception as e:
-        print("Error occurred:", e)
-        return None
-
-
-def make_Google_sheets_2024(file):
     try:
         # Make dataframe
         df = pd.read_excel(file)
@@ -70,12 +33,13 @@ def make_Google_sheets_2024(file):
         columnsToDrop = [3, 6, 11, 14, 17, 20, 23, 26]
         df = df.drop(df.columns[columnsToDrop], axis=1)
 
-        if file == 'before0710original2024.xlsx':
+        if file == files[-1]:
             name = 20241
         else:
-            name = 2024
+            name = file[:5]
 
         return name, df
+
     except Exception as e:
         print("Error occurred:", e)
         return None
@@ -84,32 +48,15 @@ def make_Google_sheets_2024(file):
 # Opening the workbook
 book = general_functions.openGoogle()
 
-# url for Excel data from Finance Ministry
-# These url were taken from 'https://www.gov.il/he/departments/policies/tableau'
-# url2024 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2024.xls'
-url2023 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2023.xlsx'
-url2022 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2022.xlsx'
-url2021 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_tableau_BudgetData2021.xlsx'
-url2020 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2020.xlsx'
-url2019 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2019.xlsx'
-url2018 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2018.xlsx'
-url2017 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2017.xlsx'
-url2016 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2016.xlsx'
-url2015 = 'https://www.gov.il/BlobFolder/policy/tableau/he/tableau_BudgetData2015.xlsx'
-# url20241 = 'C:\\Users\danny\Desktop\BudgetData\original2024Before0710231024.xlsx'
-
-url_list = [url2015, url2016, url2017, url2018, url2019, url2020, url2021, url2022, url2023]
-files = ['finance_ministry_data/before0710original2024.xlsx', 'finance_ministry_data/new2024.xlsx']
-
-# for url in url_list:
-#     name, df = make_Google_sheets(url)
-#     print(name)
-#     book.add_worksheet(title=name, rows=len(df)+1, cols=len(df.columns)+1)
-#     set_with_dataframe(book.name, df)
-
+files = ['finance_ministry_data/2015.xlsx', 'finance_ministry_data/2016.xlsx',
+         'finance_ministry_data/2017.xlsx', 'finance_ministry_data/2018.xlsx',
+         'finance_ministry_data/2019.xlsx', 'finance_ministry_data/2020.xlsx',
+         'finance_ministry_data/2021.xlsx', 'finance_ministry_data/2022.xlsx',
+         'finance_ministry_data/2023.xlsx', 'finance_ministry_data/new2024.xlsx',
+         'finance_ministry_data/before0710original2024.xlsx']
 
 for file in files:
-    name, df = make_Google_sheets_2024(file)
+    name, df = make_Google_sheets(file)
     # if worksheet exist then delete and make new with the same name with updated data
     try:
         if book.worksheet(f'{name}'):
