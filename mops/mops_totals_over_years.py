0c2707266bcef8297f9f45a96eb0f1e3dd115913
dkrivaa <dannykrivaa@hotmail.com>
2024-05-20 11:57:07 +0300

    make mops data

--- mops/mops_totals_over_years.py
+++ mops/mops_totals_over_years.py
@@ -34,16 +34,16 @@ df_list = [df_ministry, df_witness, df_police, df_prison, df_fire]
 #                     for budget_type in types]
 # print('total budgets', total_budget_sums)
 
-test_sum = df_ministry[df_ministry.iloc[:, 15].isin(wage_codes) | df_ministry.iloc[:, 17].isin(wage_codes)].iloc[:, 22].sum()
-print(test_sum)
+# test_sum = df_ministry[df_ministry.iloc[:, 15].isin(wage_codes) | df_ministry.iloc[:, 17].isin(wage_codes)].iloc[:, 22].sum()
+# print(test_sum)
 
-# total_wage_sums = [(year, budget_type, data[(data.iloc[:, 0] == int(year)) &
-#                                     (data.iloc[:, 21] == budget_type) &
-#                                     (data[data.iloc[:, 15].isin(wage_codes) | data[data.iloc[:, 17].isin(wage_codes)]])]
-#                                     .iloc[:, 22].sum())
-#                      for data in df_list
-#                      for year in available_years
-#                      for budget_type in types]
+total_wage_sums = [(year, budget_type, data[(data.iloc[:, 0] == int(year)) &
+                    (data.iloc[:, 21] == budget_type) &
+                    (data[data.iloc[:, 15].isin(wage_codes) | [data.iloc[:, 17].isin(wage_codes)]])]
+                    .iloc[:, 22].sum())
+                 for data in df_list
+                 for year in available_years
+                 for budget_type in types]
 #
 # print('total wages', total_wage_sums)
 
