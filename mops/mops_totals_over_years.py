2957d9b24b7051acc72ee51ced82fc1423d0abed
dkrivaa <dannykrivaa@hotmail.com>
2024-05-20 07:04:06 +0300

    make mops data

--- mops/mops_totals_over_years.py
+++ mops/mops_totals_over_years.py
@@ -28,8 +28,9 @@ df_fire = df[df.iloc[:, 9].isin(ministry_codes[4])]
 df_list = [df_ministry, df_witness, df_police, df_prison, df_fire]
 
 for data in df_list:
-    budget = data.iloc[:, 22].sum()
-    print(budget)
+    for year in available_years:
+        budget = data[data.iloc[:, 0] == year].iloc[:, 22].sum()
+        print(budget)
 
 # budget_sums = [(year, budget_type, data[(data.iloc[:, 0] == year) &
 #                                         (data.iloc[:, 21] == budget_type)].iloc[:, 22].sum())
