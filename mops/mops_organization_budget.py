64e71acdc1be93d432a6118e992982eb2f91ccc3
dkrivaa <dannykrivaa@hotmail.com>
2024-05-21 08:17:17 +0300

    make mops data

--- mops/mops_organization_budget.py
+++ mops/mops_organization_budget.py
@@ -20,8 +20,8 @@ organization_code = sys.argv[1]
 
 organization = 'ministry'
 if organization_code is not None:
-    organization_list = ['ministry', 'witness', 'police', 'prison', 'fire']
-    organization = organization_list[int(organization_code)-1]
+    # organization_list = ['ministry', 'witness', 'police', 'prison', 'fire']
+    # organization = organization_list[int(organization_code)-1]
     print('organization_code', organization_code)
 
 else:
