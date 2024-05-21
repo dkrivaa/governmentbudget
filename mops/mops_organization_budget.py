77e03dddb25f6ea5ad747682fd2ef825c351fd12
dkrivaa <dannykrivaa@hotmail.com>
2024-05-21 08:32:46 +0300

    make mops data

--- mops/mops_organization_budget.py
+++ mops/mops_organization_budget.py
@@ -12,31 +12,33 @@ import itertools
 
 # Check if the command-line argument is provided
 if len(sys.argv) < 2:
+    print(sys.argv)
     print("Usage: python mops_organization_budget.py ORGANIZATION")
     sys.exit(1)
 
-# Get the value of the ORGANIZATION argument
+# Get the value of the ORGANIZATION argument chosen by user
 organization_code = sys.argv[1]
+organization = ''
 
-organization = 'ministry'
 if organization_code is not None:
     organization_list = ['ministry', 'witness', 'police', 'prison', 'fire']
-    organization = organization_list[int(organization_code)-1]
+    organization = organization_list[int(organization_code)]
     print('organization', organization)
-
 else:
     print('code not passed')
 
-# # Opening workbook
-# book, available_years = general_functions.openGoogle()
-# # Ministry codes for the various orgs in ministry (column index 9) - returns list with lists of codes
-# ministry_codes = general_functions.mops_codes()
-# # wage codes - columns index 15 and 17 - returns list of codes
-# wage_codes = general_functions.wage_codes()
-# # budget types - returns list of lists of codes
-# types = general_functions.budget_types()
-#
-# # making dataframe with all mops data for all years
-# df = get_as_dataframe(book.worksheet('mops'))
+# Opening workbook
+book, available_years = general_functions.openGoogle()
+# Ministry codes for the various orgs in ministry (column index 9) - returns list with lists of codes
+ministry_codes = general_functions.mops_codes()
+# wage codes - columns index 15 and 17 - returns list of codes
+wage_codes = general_functions.wage_codes()
+# budget types - returns list of lists of codes
+types = general_functions.budget_types()
+
+
+
+# making dataframe with all mops data for all years
+df = get_as_dataframe(book.worksheet('mops'))
 
 
