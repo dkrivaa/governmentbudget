2504a23f41a42592321f5c46ef5ffcde6c85442c
dkrivaa <dannykrivaa@hotmail.com>
2024-05-22 07:09:46 +0300

    make mops data

--- macro/macro_totals_over_years.py
+++ macro/macro_totals_over_years.py
@@ -11,28 +11,42 @@ from general import general_functions
 # Opening workbook
 book, available_years = general_functions.openGoogle()
 
-df_2024 = get_as_dataframe(book.worksheet('2024'))
-# setting dataframe column names
-column_1 = df_2024.columns[1]
-column_2 = df_2024.columns[2]
-column_3 = df_2024.columns[3]
-column_4 = df_2024.columns[4]
-column_21 = df_2024.columns[21]  # type of budget (original, approved, executed)
-column_22 = df_2024.columns[22]  # net budget
+def macro_for_year(year):
+    year = str(year)
+    df = get_as_dataframe(book.worksheet(year))
+    # setting dataframe column names
+    column_1 = df.columns[1]
+    column_2 = df.columns[2]
+    column_3 = df.columns[3]
+    column_4 = df.columns[4]
+    column_21 = df.columns[21]  # type of budget (original, approved, executed)
+    column_22 = df.columns[22]  # net budget
 
-level1_budgets = [[group[0], group[1],
-                    group_df.loc[group_df[column_21] == 'מקורי', column_22].sum()]
-                    for group, group_df in df_2024.groupby([column_1, column_2])]
-df_level1 = pd.DataFrame(level1_budgets, columns=['code1', 'area1', 'budget'])
+    level1_budgets = [[group[0], group[1],
+                        group_df.loc[group_df[column_21] == 'מקורי', column_22].sum()]
+                        for group, group_df in df.groupby([column_1, column_2])]
+    df_level1 = pd.DataFrame(level1_budgets, columns=['code1', 'area1', f'budget{year}'])
 
-level2_budgets = [[group[0], group[1], group[2], group[3],
-                    group_df.loc[group_df[column_21] == 'מקורי', column_22].sum()]
-                    for group, group_df in df_2024.groupby([column_1, column_2, column_3, column_4])]
-df_level2 = pd.DataFrame(level2_budgets, columns=['code1', 'area1', 'code2', 'area2', 'budget'])
+    level2_budgets = [[group[0], group[1], group[2], group[3],
+                        group_df.loc[group_df[column_21] == 'מקורי', column_22].sum()]
+                        for group, group_df in df.groupby([column_1, column_2, column_3, column_4])]
+    df_level2 = pd.DataFrame(level2_budgets, columns=['code1', 'area1', 'code2', 'area2', f'budget{year}'])
 
-# writing results to google sheet
-sheet = book.worksheet('results')
-set_with_dataframe(sheet, df_level1)
-sheet.format('C', {'numberFormat': {'type': 'NUMBER', 'pattern': '#,###'}})
-set_with_dataframe(sheet, df_level2, row=11, col=1)
-sheet.format('E', {'numberFormat': {'type': 'NUMBER', 'pattern': '#,###'}})
+    # writing results to google sheet
+    sheet = book.worksheet('results')
+    if int(year) == 2024:
+        set_with_dataframe(sheet, df_level1)
+        sheet.format('C', {'numberFormat': {'type': 'NUMBER', 'pattern': '#,###'}})
+        set_with_dataframe(sheet, df_level2, row=11, col=1)
+        sheet.format('E', {'numberFormat': {'type': 'NUMBER', 'pattern': '#,###'}})
+    else:
+        df_level1.drop([0, 1])
+        set_with_dataframe(sheet, df_level1, row=1, col=4)
+        sheet.format('D', {'numberFormat': {'type': 'NUMBER', 'pattern': '#,###'}})
+
+        df_level2.drop([0, 1, 2, 3])
+        set_with_dataframe(sheet, df_level2, row=11, col=6)
+        sheet.format('F', {'numberFormat': {'type': 'NUMBER', 'pattern': '#,###'}})
+
+macro_for_year(2024)
+macro_for_year(20241)
